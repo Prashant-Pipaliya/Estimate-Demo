9a387830d834301d3882942f6ec07950db3a1953
Prashant Pipaliya <prashant.p.9737@gmail.com>
2024-12-05 15:27:38 +0530

    Fixed: Public directory issue.

--- src/api/estimateApi.js
+++ src/api/estimateApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const fetchEstimateData = async () => {
-  const response = await axios.get("../../public/React JS- Estimate_detail.json");
+  const response = await axios.get("/React JS- Estimate_detail.json");
   return response?.data;
 };
 
